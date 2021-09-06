fd14c8275f5e52a15986405c2b9e3a71676cbd2d
Boris <boris.mil.96@gmail.com>
2021-09-06 16:47:51 +0300

    Add static.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hbs = require('express-handlebars');
 
 const port = 5000;
 const app = express();
-app.engine('.hbs', hbs({ extname: '.hbs' }));
+app.engine('.hbs', hbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-
+app.use('/static',express.static('static'));
 app.listen(port, () => console.log(`Server is on port ${port}`));
