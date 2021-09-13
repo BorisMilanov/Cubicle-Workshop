c1b589654a7f45d2656a97b07eea974780595fe9
Boris <boris.mil.96@gmail.com>
2021-09-14 02:14:37 +0300

    Create cube

--- controllers/create.js
+++ controllers/create.js
@@ -2,7 +2,16 @@ module.exports = {
     create(req, res) {
         res.render('create.hbs',{title:'Create Cube'});
     },
-    post(req, res) {
+    post: async (req, res) => {
+        const cube = {
+            name: req.body.name,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+            dificulty: req.body.dificulty
+        }
+
+        await req.storage.create(cube);
+
         res.redirect('/');
     }
 }
--- index.js
+++ index.js
@@ -5,19 +5,24 @@ const { about } = require('./controllers/about.js');
 const { details } = require('./controllers/details.js');
 const { create, post } = require('./controllers/create.js');
 const { notFound } = require('./controllers/notFound.js');
+const { init: storage } = require('./models/storage')
 
-const port = 5000;
-const app = express();
-app.engine('.hbs', hbs({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-app.use('/static', express.static('static'));
+start();
+async function start() {
+    const port = 5000;
+    const app = express();
+    app.engine('.hbs', hbs({ extname: '.hbs' }));
+    app.set('view engine', '.hbs');
+    app.use('/static', express.static('static'));
+    app.use(await storage());
+    app.use(express.urlencoded({extended:false}))
+    app.get('/', catalog);
+    app.get('/about', about);
+    app.get('/details/:id', details);
+    app.get('/create', create);
+    app.post('/create', post);
 
-app.get('/', catalog);
-app.get('/about', about);
-app.get('/details/:id', details);
-app.get('/create', create);
-//app.post('/create', post);
+    app.all('*', notFound);
 
-app.all('*', notFound);
-
-app.listen(port, () => console.log(`Server is on port ${port}`));
+    app.listen(port, () => console.log(`Server is on port ${port}`));
+}
